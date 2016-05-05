968d96d74b4db3af147faaa85d038a2abcf58e85
Zlobin Eugene <creastar@gmail.com>
2016-05-05 13:12:57 +0600

    Update demo

--- demo/server.php
+++ demo/server.php
@@ -4,12 +4,14 @@
   $type = $_SERVER['CONTENT_TYPE'];
   $override = $_SERVER['HTTP_X_HTTP_METHOD'];
   $method = $_SERVER['REQUEST_METHOD'];
+  $uploadDir = '/home/eugene/projects/es-ajax/demo/uploads/';
 
   $response = [
     'foo' => 'bar',
     'request' => [
       'method' => $method,
       'type' => $type,
+      'isFile' => isset($_FILES, $_FILES['file']),
       // If http server doesn't allow PUT, DELETE methods.
       'isOverriden' => isset($override),
       'overrideMethod' => $override
@@ -19,6 +21,17 @@
   // For adding an extra delay in seconds.
   if ($_REQUEST['timeout']) {
     sleep((int)$_REQUEST['timeout']);
+  }
+
+  // File upload.
+  if (isset($_FILES, $_FILES['file'])) {
+    $response['request']['fileUploaded'] = false;
+    $fileName = $_FILES['file']['name'];
+    $fileTmp = $_FILES['file']['tmp_name'];
+
+    if (move_uploaded_file($file_tmp, $uploadDir . $file_name)) {
+      $response['request']['fileUploaded'] = true;
+    }
   }
 
   header('Content-Type: ' . $type . '; charset=utf-8');
