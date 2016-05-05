86aa372e8c824b12ecc80f13424648041af6a1ae
Zlobin Eugene <creastar@gmail.com>
2016-05-05 13:12:17 +0600

    Exclide is as utils

---
+++ src/utils/is.js
@@ -0,0 +1,17 @@
+export const is = {
+  _undefined(value) {
+    return value === void 0;
+  },
+
+  _object(value) {
+    return typeof value === 'object';
+  },
+
+  _function(value) {
+    return typeof value === 'function';
+  },
+
+  _array(value) {
+    return value instanceof Array;
+  }
+};
